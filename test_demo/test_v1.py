488d18e9d2abe614f5322b801aa0e62b6f808e9a
Geoff Daly <geoff.daly@moto-way.co.uk>
2025-11-11 15:46:06 +0000

    test examples

--- test_demo/test_v1.py
+++ test_demo/test_v1.py
@@ -4,8 +4,20 @@ from calculator_app import Calculator
 class TestOperations(unittest.TestCase):
     
     def test_sum(self):
-        calc = Calculator(2,8)
-        self.assertEqual(calc.get_sum(), 10, "the sum is wrong")
+        calc = Calculator(10,2)
+        self.assertEqual(calc.get_sum(), 12, "the sum is wrong")
+
+        def test_difference(self):
+        calc = Calculator(10,2)
+        self.assertEqual(calc.get_difference(), 8, "the sum is wrong")
+
+        def test_quotient(self):
+        calc = Calculator(10,2)
+        self.assertequal(calc.get_quotient(), 5, "the sum is wrong")
+
+        def test_sum(self):
+        calc = Calculator(10,2)
+        self.assertEqual(calc.get_product(), 20, "the sum is wrong")    
 
 if __name__ == "__main__":
     unittest.main()        
